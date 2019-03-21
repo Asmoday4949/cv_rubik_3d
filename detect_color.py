6e78a9e5e6dfd99b68a6e41fd5cf67fd53e3ee27
bull0n <lucas.bulloni@protonmail.ch>
2019-03-21 11:16:26 +0100

    detecting the right color

--- detect_color.py
+++ detect_color.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2 as cv
+from color_enum import Color
 
 def detect_color(image):
     square_zone = [[170, 500], 480] # x;y -> left corner , w;h
@@ -10,10 +11,11 @@ def detect_color(image):
 
     sub_zone = [square_zone[0].copy(), width_one_square]
 
-    result = [][]
+    result = []
 
     for i in range(0,3):
         sub_zone[0][0] = square_zone[0][0]
+        result.append([])
 
         for j in range(0,3):
             start_width = sub_zone[0][1] + MARGIN
@@ -24,8 +26,8 @@ def detect_color(image):
 
             subimage = image[start_width:end_width, start_height:end_height]
 
-            bgr_mean = get_bgr_value_subimage(subimage)
-            result[i][j] = decide_color(bgr_mean)
+            bgr_mean = get_bgr_value_subimage(subimage, np.mean)
+            result[i].append(decide_color(bgr_mean))
 
             cv.imshow(f"{i}:{j}", subimage)
             sub_zone[0][0] += width_one_square
@@ -33,14 +35,29 @@ def detect_color(image):
         sub_zone[0][0] = square_zone[0][0]
         sub_zone[0][1] += width_one_square
 
-def decide_color(bgr_value):
-    pass
+    return result
 
-def get_bgr_value_subimage(subimage):
+def decide_color(bgr_value):
+    if bgr_value.mean() > 150 and np.std(bgr_value) < 40 :
+        return Color.WHITE
+    elif bgr_value[0] > 100 and bgr_value[1] < 60 and bgr_value[2] < 60:
+        return Color.BLUE
+    elif bgr_value[0] < 60 and bgr_value[1] > 110 and bgr_value[2] < 60:
+        return Color.GREEN
+    elif bgr_value[0] > 30 and bgr_value[0] < 60 and bgr_value[1] < 60 and bgr_value[2] > 150:
+        return Color.RED
+    elif bgr_value[0] < 20 and bgr_value[1] > 50 and bgr_value[1] > 100 and bgr_value[2] > 170:
+        return Color.YELLOW
+    elif bgr_value[0] < 20 and bgr_value[1] > 50 and bgr_value[1] < 100 and bgr_value[2] > 170:
+        return Color.ORANGE
+
+    return None
+
+def get_bgr_value_subimage(subimage, math_func):
     bgr_mean = np.empty((3,))
 
     for i in range(0,3):
-        bgr_mean[i] = subimage[:,:,i].mean()
+        bgr_mean[i] = math_func(subimage[:,:,i])
 
     return bgr_mean
 
@@ -48,6 +65,6 @@ if __name__ == '__main__':
     img = cv.imread('img/01.jpg', cv.IMREAD_COLOR)
     print(img.shape)
     cv.imshow(f"source", img)
-    detect_color(img)
+    print(detect_color(img))
     cv.waitKey(0)
     cv.destroyAllWindows()
