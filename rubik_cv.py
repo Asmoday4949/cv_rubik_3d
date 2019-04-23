36b7bb7317c14a43d396b1a6b17166fba2e4ec0e
Asmoday4949 <malik.fleury@he-arc.ch>
2019-04-23 15:18:33 +0200

    Remove comments

--- rubik_cv.py
+++ rubik_cv.py
@@ -59,15 +59,15 @@ def detect_faces(square_zone = None):
     return faces
 
 
-#import kociemba
+import kociemba
 
 def rubik_cv():
-    #faces = detect_faces()
-    #cube = reconstruct(faces)
-    #solution = kociemba.solve(cube)
-    #print(faces)
-    #print(cube)
-    #print(solution)
+    faces = detect_faces()
+    cube = reconstruct(faces)
+    solution = kociemba.solve(cube)
+    print(faces)
+    print(cube)
+    print(solution)
 
     gl_app = OpenGLApp("F B")
     gl_app.run()
