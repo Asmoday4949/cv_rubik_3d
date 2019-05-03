cfb09975029ef76d41cd04a5beac0c6a10494cea
Bastien Wermeille <bastien.wermeille@gmail.com>
2019-05-03 17:11:39 +0200

    Fix issue in reconstruct + rapport

--- documentation/rapport.md
+++ documentation/rapport.md
@@ -30,14 +30,13 @@ header-includes: |
 
 # Configuration d'exécution
 
-Pour la bonne exécution du programme, il vous installer le matériel dans la manière optimale.
+Pour la bonne exécution du programme, il vous installer le matériel **dans** la manière optimale.
 
 ## Matérielle nécessaire
 
 - Caméra
 - Rubik's cube
-
-**Optionnel :** Lampe
+- *Optionnel :* Lampe
 
 La lampe permet d'avoir un meilleur résultat pour la détection de la position du cube et la détection des couleurs.
 
@@ -45,8 +44,72 @@ Le but de ce projet était la réalisation d'une application permettant la déte
 
 # Configuration du programme
 
-Ce programme nécessite les packages python suivantes:
-- 
+Ce programme nécessite un certain nombre de package python. Nous avons utilisé un `venv` durant le dévelopement afin de faciliter l'intallation. Le fichier `requirements.txt` contient la liste des dépendances, pour créer le venv et installer celles-ci sur un système linux, vous pouvez executer les lignes de commandes suivantes. 
+
+```sh
+TODO: Ajouter VENV command lines
+```
+
+En ce qui concerne les dépendances externes, il est nécessaire d'installer la bibliothèque:
+- kociemba
+
+Installable de la manière suivante :
+
+```sh
+sudo apt install kociemba
+```
+
+# Fonctionnement et Architecture
+
+Ce projet a été séparé en 5 étapes distinctes qui sont les suivantes:
+
+1. Détection d'une face du rubik's cube
+2. Reconnaissance des couleurs de la face
+3. Reconstruction du rubik's cube valide à partir des 6 faces scannées dans n'importe quel ordre
+4. Résolution du rubik's cube
+5. Affichage du rubik's cube en 3D
+
+## Détection d'une face du rubik's cube
+
+
+TODO: Bastien
+
+## Reconnaissance des couleurs d'une face
+
+TODO: Lucas
+
+## Reconstruction du rubik's cube
+
+Les deux étapes précédentes permettant d'isoler chaque face et de reconnaitre leurs différentes couleurs, l'étape suivante était de remettre dans l'ordres les différentes faces du cube afin que celui-ci soit valide.
+
+Cette étape consistait en deux points essentiels:
+1. Détecter si l'ensemble des faces permettaient de reconstruire un  rubik's cube valide
+2. Reconstruire le rubik's cube
+
+Lors du développement nous avons écris nos propres tests afin de tester si le cube était valide notemment en comptant le nombre de couleurs totales mais celà ne suffisait pas alors nous avons décider d'utiliser la bibliothèque `kociemba` permettant de valider si un rubik's cube était valide.
+
+Une fois la possibilité de pouvoir tester si un rubik's cube était valide, il nous restait à trouver comment agencer les différentes faces.
+
+Après un certains nombre de tests avec la bibliothèque pour vérifier la validité du cube, nous nous sommes rendu compte que cette méthode était extrêmement rapide et que le meilleur moyen de trouver la bonne configuration était de proposer chaque solution à notre fonction.
+
+Cette opération peut paraître conteuse mais le nombre de cas à tester est très petit: `4*4*4*4*4*4 = 5096`. Ce calcul découle du fait que le point central de chaque face ne bouge jamais et donc que nous savons quelles faces sont adjacentes. Il ne nous reste plus qu'à tester le composition des 4 rotations possibibles de chaque faces. Tester ces 4096 possibilités est extrêment rapide et dès que la solution est trouvée, le processus s'arrête. Il n'existe qu'une seule possibilité d'assemblage des différentes faces pour chaque cube. Le seul cas ou on peut avoir plusieurs faces valides est lorsque une face du rubik's cube est uniforme ou lorsqu'elle est symétrique.
+
+## Résolution du rubik's cube
+
+La résolution du rubik's n'a pas étée développée par nos soins car ce travail n'était pas le point central du projet. Nous avons utilisé l'algporithme de Kociemba afin d'effectuer cette étape.
+
+Le code nécessaire a cette partie est relativement simple et est le suivant:
+```python
+import kociemba
+solution = kociemba.solve(cube)
+```
+
+La variable cube étant une string définissant l'état du cube par exemple:
+- `BBRUUUUDFBRUURRBBDLFFFFFRFBDLFDDDFDDLBDLLLRLRLBUUBRURL`.
+
+## Affichage du rubik's cube en 3D
+
+TODO: Malik
 
 # Utilisation
 
--- reconstruct.py
+++ reconstruct.py
@@ -34,23 +34,25 @@ def reconstruct(rubik):
         faces[face[1][1]] = np.transpose(np.array(face))
     print(faces)
 
-    for blue in range(0,4):
-        for red in range(0,4):
-            for orange in range(0,4):
-                for green in range(0,4):
-                    for yellow in range(0,4):
-                        cube = cube_to_string(faces)
-                        valid = tools.verify(cube)
-                        if valid == 0:
-                            print("Found it")
-                            print(valid)
-                            print(cube)
-                            return cube
-                        faces[Color.YELLOW] = np.rot90(faces[Color.YELLOW])
-                    faces[Color.GREEN] = np.rot90(faces[Color.GREEN])
-                faces[Color.ORANGE] = np.rot90(faces[Color.ORANGE])
-            faces[Color.RED] = np.rot90(faces[Color.RED])
-        faces[Color.BLUE] = np.rot90(faces[Color.BLUE])
+    for white in range(0,4):
+        for blue in range(0,4):
+            for red in range(0,4):
+                for orange in range(0,4):
+                    for green in range(0,4):
+                        for yellow in range(0,4):
+                            cube = cube_to_string(faces)
+                            valid = tools.verify(cube)
+                            if valid == 0:
+                                print("Found it")
+                                print(valid)
+                                print(cube)
+                                return cube
+                            faces[Color.YELLOW] = np.rot90(faces[Color.YELLOW])
+                        faces[Color.GREEN] = np.rot90(faces[Color.GREEN])
+                    faces[Color.ORANGE] = np.rot90(faces[Color.ORANGE])
+                faces[Color.RED] = np.rot90(faces[Color.RED])
+            faces[Color.BLUE] = np.rot90(faces[Color.BLUE])
+        faces[Color.WHITE] = np.rot90(faces[Color.WHITE])
 
     cube = cube_to_string(faces)
 
@@ -66,36 +68,12 @@ if __name__ == '__main__':
     # print(tools.verify("BBRUUUUDFBRUURRBBDLFFFFFRFBDLFDDDFDDLBDLLLRLRLBUUBRURL"))
     # print(kociemba.solve("BBRUUUUDFBRUURRBBDLFFFFFRFBDLFDDDFDDLBDLLLRLRLBUUBRURL"))
     
-    test = [
-        [
-            [Color.ORANGE,Color.WHITE,Color.WHITE],
-            [Color.WHITE,Color.WHITE,Color.WHITE],
-            [Color.RED,Color.WHITE,Color.YELLOW]
-        ],
-        [
-            [Color.YELLOW,Color.RED,Color.BLUE],
-            [Color.BLUE,Color.RED,Color.RED],
-            [Color.YELLOW,Color.YELLOW,Color.GREEN]
-        ],
-        [
-            [Color.ORANGE,Color.YELLOW,Color.BLUE],
-            [Color.BLUE,Color.YELLOW,Color.RED],
-            [Color.BLUE,Color.RED,Color.ORANGE]
-        ],
-        [
-            [Color.ORANGE,Color.YELLOW,Color.GREEN],
-            [Color.ORANGE,Color.ORANGE,Color.ORANGE],
-            [Color.RED,Color.ORANGE,Color.RED]
-        ],
-        [
-            [Color.YELLOW,Color.YELLOW,Color.RED],
-            [Color.BLUE,Color.BLUE,Color.BLUE],
-            [Color.BLUE,Color.GREEN,Color.WHITE]
-        ],
-        [
-            [Color.GREEN,Color.ORANGE,Color.WHITE],
-            [Color.GREEN,Color.GREEN,Color.GREEN],
-            [Color.WHITE,Color.GREEN,Color.GREEN]
-        ]
-    ]
+    # test = [[[Color.YELLOW, Color.YELLOW, Color.YELLOW], [Color.BLUE, Color.GREEN, Color.ORANGE], [Color.WHITE, Color.BLUE, Color.GREEN]], [[Color.ORANGE, Color.YELLOW, Color.ORANGE], [Color.GREEN, Color.RED, Color.ORANGE], [Color.ORANGE, Color.BLUE, Color.BLUE]], [[Color.GREEN, Color.RED, Color.YELLOW], [Color.WHITE, Color.BLUE, Color.RED], [Color.GREEN, Color.GREEN, Color.BLUE]], [[Color.RED, Color.BLUE, Color.YELLOW], [Color.GREEN, Color.ORANGE, Color.ORANGE], [Color.RED, Color.WHITE, Color.ORANGE]], [[Color.BLUE, Color.ORANGE, Color.GREEN], [Color.GREEN, Color.WHITE, Color.YELLOW], [Color.BLUE, Color.YELLOW, Color.RED]], [[Color.WHITE, Color.RED, Color.RED], [Color.WHITE, Color.YELLOW, Color.WHITE], [Color.WHITE, Color.RED, Color.WHITE]]]
+    # test = [[[Color.GREEN, Color.GREEN, Color.GREEN], [Color.GREEN, Color.GREEN, Color.GREEN], [Color.GREEN, Color.GREEN, Color.GREEN]], [[Color.RED, Color.RED, Color.RED], [Color.RED, Color.RED, Color.RED], [Color.RED, Color.RED, Color.RED]], [[Color.BLUE, Color.BLUE, Color.BLUE], [Color.BLUE, Color.BLUE, Color.BLUE], [Color.BLUE, Color.BLUE, Color.BLUE]], [[Color.ORANGE, Color.ORANGE, Color.ORANGE], [Color.ORANGE, Color.ORANGE, Color.ORANGE], [Color.ORANGE, Color.ORANGE, Color.ORANGE]], [[Color.WHITE, Color.WHITE, Color.WHITE], [Color.WHITE, Color.WHITE, Color.WHITE], [Color.WHITE, Color.WHITE, Color.WHITE]], [[Color.YELLOW, Color.YELLOW, Color.YELLOW], [Color.YELLOW, Color.YELLOW, Color.YELLOW], [Color.YELLOW, Color.YELLOW, Color.YELLOW]]]
+    test = [[[Color.WHITE, Color.WHITE, Color.WHITE], [Color.GREEN, Color.GREEN, Color.GREEN], [Color.GREEN, Color.GREEN, Color.GREEN]],
+     [[Color.RED, Color.RED, Color.RED], [Color.RED, Color.RED, Color.RED], [Color.RED, Color.RED, Color.RED]], 
+     [[Color.YELLOW, Color.YELLOW, Color.YELLOW], [Color.BLUE, Color.BLUE, Color.BLUE], [Color.BLUE, Color.BLUE, Color.BLUE]],
+      [[Color.ORANGE, Color.ORANGE, Color.ORANGE], [Color.ORANGE, Color.ORANGE, Color.ORANGE], [Color.ORANGE, Color.ORANGE, Color.ORANGE]], 
+      [[Color.BLUE, Color.BLUE, Color.BLUE], [Color.WHITE, Color.WHITE, Color.WHITE], [Color.WHITE, Color.WHITE, Color.WHITE]], 
+      [[Color.GREEN, Color.GREEN, Color.GREEN], [Color.YELLOW, Color.YELLOW, Color.YELLOW], [Color.YELLOW, Color.YELLOW, Color.YELLOW]]]
     print(reconstruct(test))
